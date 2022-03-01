6fa56eb0b47c5e52b164b7760a833520099f4201
haruiino <wintersu100@gmail.com>
2022-03-01 15:51:52 +0900

    Initial Commit

---
+++ HealthRes/HealthResApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthResApp.swift
+//  HealthRes
+//
+//  Created by Haru Iino on 2022/03/01.
+//
+
+import SwiftUI
+
+@main
+struct HealthResApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
